dee97babf2bfd3be2cf5d5edad0eeed815f11a0c
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-04-06 04:11:52 +0530

    UC1 Added

---
+++ AddressProblems/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_book
+{
+     class Contact
+    {
+        public static void contact_Details()
+        {
+            Console.WriteLine("Enter the First Name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter the Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter the City Name");
+           string City = Console.ReadLine();
+            Console.WriteLine("Enter the State Name");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter the zip code");
+            string Zip = Console.ReadLine();
+            Console.WriteLine("Enter the Phone Number");
+            string PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter the email address");
+            string Email = Console.ReadLine();
+        }
+
+    }
+}
---
+++ AddressProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Address_book
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Address Book System Program!");
+            Contact.contact_Details();
+        }
+    }
+
+}
+
